5648930f495655acf9cd33faab21900e4ac19dc6
aniktalukder <aniktalukder27@gmail.com>
2024-02-04 00:24:03 +0530

    Minor - Request mapping added

--- src/main/java/com/example/policysystem/controller/PolicyApi.java
+++ src/main/java/com/example/policysystem/controller/PolicyApi.java
@@ -12,11 +12,13 @@ import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Objects;
 
 @RestController
+@RequestMapping("/api/v1")
 public class PolicyApi {
 
     Logger logger = LoggerFactory.getLogger(PolicyApi.class);
